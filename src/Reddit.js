0f5eb835f2c32083803cce1fed3c3dfde53b07b9
zunair <zunairex@gmail.com>
2020-06-07 14:11:01 +0500

    initial Commit

---
+++ src/Reddit.js
@@ -0,0 +1,32 @@
+import React,{useState} from "react";
+import axios from "axios";
+
+function Reddit(){
+    const [posts,setPosts]=useState([]);
+    React.useEffect(() => {
+        axios.get(`https://www.reddit.com/r/reactjs.json`)
+          .then(res => {
+            const newPosts = res.data.data.children
+              .map(obj => obj.data);
+      
+            setPosts(newPosts);
+          });
+      }, []);
+    return (
+        <div>
+            <ul>
+                {posts.map(post=>(
+                <li key={post.id}>
+                    
+                    <a href={post.url} target="_blank"  rel="noopener noreferrer"><h3>{post.title}</h3></a>
+                    <h4>Score {post.score}</h4>
+                    <h5>Posted by {post.author}</h5>
+                </li>)
+                )}
+            </ul>
+        </div>
+
+        
+    )
+}
+export default Reddit;
